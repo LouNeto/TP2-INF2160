415ff54a77ab384d99f56e4e2b380e77228bc5b1
LouNeto <lou.g.n@hotmail.com>
2017-04-22 18:52:36 -0400

    Overhaul

--- TP1.pl
+++ TP1.pl
@@ -3,58 +3,63 @@
 % NETL14039105, DECP09059005
 % -----------------------------------------------------------------------------
 
-% Elements possibles dans les maisons
-% P = 1, 2, 3, 4, 5
-% C = rouge, jaune, bleu, verte, ivoire
-% N = anglais, espagnol, norvegien, ukrainien, canadien
-% A = chien, renard, serpent, cheval, zebre
-% B = jus_orange, the, cafe, lait, eau
-% M = kiwi, celeri, wasabi, litchi, poire
+% question(P, C, N, A, B, M) :-
+%   P = [1, 2, 3, 4, 5],
+%   C = [rouge, jaune, bleu, verte, ivoire],
+%   N = [anglais, espagnol, norvegien, ukrainien, canadien],
+%   A = [chien, renard, serpent, cheval, zebre],
+%   B = [jus_orange, the, cafe, lait, eau],
+%   M = [kiwi, celeri, wasabi, litchi, poire].
+
+
+main :-
+  composition(Rue).
+
+composition(Rue) :-
+
+  Rue = [[1, _, _, _, _, _],
+         [2, _, _, _, _, _],
+         [3, _, _, _, _, _],
+         [4, _, _, _, _, _],
+         [5, _, _, _, _, _]],
+
+         write_maisons(Rue).
+
+write_maisons([]).
+write_maisons([H|T]) :-
+  write(H), nl,
+  write_maisons(T).
+
+memeMaison([]).
+memeMaison(X,Y,[H|T]) :-
+  meme(X,Y),
+  member(X,H),
+  member(Y,H),
+  memeMaison(X,Y,T).
+
+
+meme( anglais, rouge ).
+meme( chien, espagnol ).
+meme( norvegien, 1 ).
+meme( kiwi, jaune ).
+meme( wasabi, serpent ).
+meme( litchi, jus_orange ).
+meme( ukrainien, the ).
+meme( canadien, poire ).
+meme( cafe, verte ).
+meme( lait, 3 ).
+
+voisin( celeri, renard ).
+voisin( norvegien, bleu ).
+voisin( kiwi, cheval ).
+voisin( verte, ivoire ).
+
+droite( verte, ivoire ).
+
+% ------------------------------------------------------------------------------
+
+% c1 :- meme(X,Y).
+% question(X,Y,Z,A,B,C) :- C1 meme;
 
 % Informations d'une maison
 % maison( Position, Couleur, Nationnalite, Animal, Breuvage, Met )
-
-% Si deux elements ont un lien et s'ils se trouvent dans la meme maison.
-% meme(Elem1, Elem2) :-
-
-% Regles sur le positionnement d'une maison par rapport a une seconde maison.
-% droite(Maison1, Maison2, LaRue) :- Maison1 > Maison2.
-% gauche(Maison1, Maison2, LaRue) :- Maison1 < Maison2.
-% voisin(Maison1, Maison2, LaRue) :- Maison1 is Maison2-1; Maison1 is Maison2+1.
-
-% afficherRue([]).
-% afficherRue([A|B]) :-
-%   show(A),
-%   afficherRue(B).
-
-regle(X, L) :- member(X, L).
-% voisin(X, Y, L) :- nextto(X, Y, L); nextto(Y, X, L).
-
-% Ensembles de regles concernant la composition des maisons.
-composition(Rue) :-
-    Rue = [maison(1, _, _, _, _, _),
-           maison(2, _, _, _, _, _),
-           maison(3, _, _, _, _, _),
-           maison(4, _, _, _, _, _),
-           maison(5, _, _, _, _, _)],
-    regle(maison(_, rouge, anglais, _, _, _), Rue),
-    regle(maison(_, _, espagnol, chien, _, _), Rue),
-    regle(maison(1, _, norvegien, _, _, _), Rue),
-    regle(maison(_, jaune, _, _, _, kiwi), Rue),
-    regle(maison(_, _, _, _, _, celeri), Rue),
-    regle(maison(_, bleu, _, _, _, _), Rue),
-    % voisin(maison(_, bleu, _, _, _, _), maison(_, _, norvegien, _, _, _), Rue),
-    % voisin(maison(_, _, norvegien, _, _, _), maison(_, bleu, _, _, _, _), Rue),
-    regle(maison(_, _, _, serpent, _, wasabi), Rue),
-    regle(maison(_, _, _, _, jus_orange, litchi), Rue),
-    regle(maison(_, _, ukrainien, _, the, _), Rue),
-    regle(maison(_, _, canadien, _, _, poire), Rue),
-    regle(maison(_, _, _, cheval, _, _), Rue),
-    % voisin(maison(_, _, _, _, _, kiwi), maison(_, _, _, cheval, _, _), Rue),
-    % voisin(maison(_, _, _, cheval, _, _), maison(_, _, _, _, _, kiwi), Rue),
-    regle(maison(_, verte, _, _, cafe, _), Rue),
-    regle(maison(_, ivoire, _, _, _, _), Rue),
-    % droite(maison(_, verte, _, _, _, _), (_, ivoire, _, _, _, _), Rue),
-    % voisin(maison(_, verte, _, _, _, _), (_, ivoire, _, _, _, _), Rue),
-    regle(maison(3, _, _, _, lait, _), Rue),
-    regle(maison(_, _, _, zebre, _, _), Rue).
